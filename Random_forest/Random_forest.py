c8c686ce427087d7c68ad18fff0f7f0dfd2a02a4
Rajkumar <rajmr6751@gmail.com>
2026-03-20 19:57:39 +0530

    first commit

---
+++ Random_forest/Random_forest.py
@@ -0,0 +1,14 @@
+from sklearn.ensemble import RandomForestClassifier
+import numpy as np
+
+X = np.array([[1], [2], [3], [4], [5]])
+
+y = np.array([0, 0, 0, 1, 1])
+
+model = RandomForestClassifier(n_estimators=10)
+
+model.fit(X, y)
+
+prediction = model.predict([[3.5]])
+
+print(prediction)
